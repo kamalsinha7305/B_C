2d33c4bbcf29e605225dd35e248c548e0faba8cf
kamal <kamalsinha7305@gmail.com>
2026-02-02 13:38:04 +0530

    new_comit

--- frontend/src/components/Footer.jsx
+++ frontend/src/components/Footer.jsx
@@ -1,10 +1,16 @@
-import React from 'react'
+import React from 'react';
+import { FaFacebook,FaInstagram ,FaLinkedin} from "react-icons/fa";
 
 function Footer() {
   return (
     <footer className='border-t'>
-      <div className='container mx-auto p-4 border-red-400 bg-amber-400'>
+      <div className='container mx-auto p-4 text-center flex flex-col gap-2'>
       <p> © All Rights Reserved 2026</p>
+      <div className='flex items-center justify-center gap-4 text-2xl'>
+        <a href="" className=' hover:text-primary-200'><FaFacebook/></a>
+        <a href="" className=' hover:text-secondary-200'><FaInstagram/></a>
+        <a href="" className=' hover:text-primary-100'><FaLinkedin/></a>
+      </div>
       </div>
 
     </footer>

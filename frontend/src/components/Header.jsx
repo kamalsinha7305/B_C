1f070b629096227d1ef4533ce86dd635f91a2c67
kamal <kamalsinha7305@gmail.com>
2026-02-05 20:08:51 +0530

    header update

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -3,51 +3,66 @@ import logo from '../assets/logo.png'
 import Search from './Search'
 import { Link } from 'react-router-dom'
 import { FaUserAstronaut } from "react-icons/fa";
+import { CiShoppingCart } from "react-icons/ci";
 import useMobile from '../hooks/useMobile';
-import {useLocation} from 'react-router-dom'
+import { useLocation } from 'react-router-dom'
 
 function Header() {
 
-  const [isMobile] =useMobile();
-    const location = useLocation();
-   
+  const [isMobile] = useMobile();
+  const location = useLocation();
 
-   const isSearchPage =location.pathname =='/search';
+
+  const isSearchPage = location.pathname == '/search';
 
 
 
   return (
     <header className='h-15 lg:h-18 lg:shadow-md sticky top-0 '>
-       {
+      {
         !(isSearchPage && isMobile) && (
           <div className='conatiner  w-full h-full mx-auto flex items-center px-2 justify-between '>
 
-        <div className='h-full'>
-          <Link to={"/"} className='h-full flex justify-center items-center '>
-            <img src={logo} width={100} height={50} alt="Logo" className='hidden lg:block'/>
-            <img src={logo} width={100} height={50} alt="Logo" className='lg:hidden '/>
-          </Link>
-  
-        </div>
+            <div className='h-full'>
+              <Link to={"/"} className='h-full flex justify-center items-center '>
+                <img src={logo} width={100} height={50} alt="Logo" className='hidden lg:block' />
+                <img src={logo} width={100} height={50} alt="Logo" className='lg:hidden ' />
+              </Link>
 
-        <div className="hidden lg:block">
-         <Search/>
-        </div>
+            </div>
 
-        <div className="">
-          <div className='lg:hidden'>
-            <FaUserAstronaut size={25}/>
+            <div className="hidden lg:block">
+              <Search />
+            </div>
+            {/*      For mobile version login icon  */}
+            <div className="">
+              <div className='lg:hidden'>
+                <FaUserAstronaut size={25} />
+              </div>
+              {/*  for destop versio login */}
+
+              <div className='hidden lg:flex items-center gap-8 '>
+                <Link to={"/login"}> Login </Link>
+                <button className='flex  items-center gap-2 bg-secondary-200 px-2 py-1 rounded text-white'>
+                  {/* add to cart icon  */}
+
+                  <div>
+                    <CiShoppingCart size={28} />
+                  </div>
+                  <div>
+                    <p> 1 items </p>
+                    <p> total price </p>
+                  </div>
+
+                </button>
+              </div>
+            </div>
+
           </div>
-          <div className='hidden lg:block'>
-          Login into the cart 
-          </div>
-        </div>
-
-      </div>
         )
-       }
+      }
       <div className='conatiner mx-auto px-2 lg:hidden ' >
-        <Search/>
+        <Search />
       </div>
 
     </header>

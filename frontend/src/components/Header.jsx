7e6ade89d59f6fd41129fedd612c83632b3c5ff9
kamal <kamalsinha7305@gmail.com>
2026-02-05 19:06:57 +0530

    mobile custom hook

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -2,10 +2,25 @@ import React from 'react'
 import logo from '../assets/logo.png'
 import Search from './Search'
 import { Link } from 'react-router-dom'
+import { FaUserAstronaut } from "react-icons/fa";
+import useMobile from '../hooks/useMobile';
+import {useLocation} from 'react-router-dom'
+
 function Header() {
+
+  const [isMobile] =useMobile();
+    const location = useLocation();
+   
+
+   const isSearchPage =location.pathname =='/search';
+
+
+
   return (
-    <header className='h-18 shadow-md sticky top-0'>
-      <div className='conatiner mx-auto flex items-center h-full px-2 justify-between'>
+    <header className='h-15 lg:h-18 lg:shadow-md sticky top-0 '>
+       {
+        !(isSearchPage && isMobile) && (
+          <div className='conatiner  w-full h-full mx-auto flex items-center px-2 justify-between '>
 
         <div className='h-full'>
           <Link to={"/"} className='h-full flex justify-center items-center '>
@@ -15,14 +30,24 @@ function Header() {
   
         </div>
 
-        <div>
+        <div className="hidden lg:block">
          <Search/>
         </div>
 
-        <div>
+        <div className="">
+          <div className='lg:hidden'>
+            <FaUserAstronaut size={25}/>
+          </div>
+          <div className='hidden lg:block'>
           Login into the cart 
+          </div>
         </div>
 
+      </div>
+        )
+       }
+      <div className='conatiner mx-auto px-2 lg:hidden ' >
+        <Search/>
       </div>
 
     </header>
--- frontend/src/components/Search.jsx
+++ frontend/src/components/Search.jsx
@@ -1,18 +1,22 @@
 import React from 'react'
-import { CiSearch } from 'react-icons/ci';
+import { CiSearch} from 'react-icons/ci';
+import { IoMdArrowBack } from "react-icons/io";
 import { TypeAnimation } from 'react-type-animation';
-import { useLocation, useNavigate } from 'react-router-dom';
+import { Link, useLocation, useNavigate } from 'react-router-dom';
 import { useState,useEffect } from 'react';
+import useMobile from '../hooks/useMobile';
 function Search() {
   const navigateTo = useNavigate();
   const location = useLocation();
 
   const [isSearchPage, setIsSearchPage] = useState(false);
+  const [isMobile] = useMobile();
+
   
   useEffect(() => {
-    const isSeacrh = location.path === '/search'
+    const isSeacrh = location.pathname === '/search'
     setIsSearchPage(isSeacrh)
-  },[isSearchPage]); 
+  },[location.pathname]); 
 
 
   const redirectToSearchPage = () => {
@@ -22,14 +26,28 @@ function Search() {
   console.log("search",isSearchPage);
  
   return (
-    <div className='  min-w-65 lg:min-w-100 lg:h-10 rounded border bg-slate-50 flex items-center  overflow-hidden cursor-pointer'>
-      <button className='flex justify-center items-center h-full p-3 text-neutral-600 '>
-        <CiSearch size={22} />
-      </button>
+    <div className='  min-w-65 lg:min-w-100 h-9 lg:h-10 rounded border bg-slate-50 flex items-center  overflow-hidden cursor-pointer  group focus-within:border-primary-100'>
+      {
+      (isMobile && isSearchPage ) ? (
+         <Link to={"/"}>
+          <button className='flex justify-center items-center h-full p-3 text-neutral-600 group-focus-within:text-primary-100 ' >
+            <IoMdArrowBack size={22} />
+          </button>
+      </Link>
+      ): (
+        
+      <div>
+          <button className='flex justify-center items-center h-full p-3 text-neutral-600 group-focus-within:text-primary-100 ' >
+            <CiSearch size={22} />
+          </button>
+      </div>
+      )
+      }
+     
 
-      <div className=" w-full h-full ">
+      <div className=" w-full h-full flex  items-center ">
         {
-        !isSearchPage?(
+        (!isSearchPage)?(
 
            <div onClick={redirectToSearchPage} >
       <TypeAnimation
